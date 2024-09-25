b61cb052cf5e045d98b0e77b19d94940c2802df3
Butyaga <127930230+Butyaga@users.noreply.github.com>
2024-09-25 06:56:25 +0300

    Update SelectProductCategory.sql

--- SelectProductCategory.sql
+++ SelectProductCategory.sql
@@ -1,10 +1,6 @@
-/*
-  В реляционной БД связи "многие ко многим", по идее, должны реализоваться через промежуточную сущность.
-  Но уж как сформулировно задание, так и сделал. Или я не так понял?
-*/
-
 SELECT
     Product.Name AS ProductName
     Category.Name AS CategoryName
-  FROM Product 
-    LEFT JOIN Category ON Product.CategoryID = Category.ID
+  FROM Product
+	LEFT JOIN ProductCategory ON Product.ID = ProductCategory.ProductID
+    LEFT JOIN Category ON ProductCategory.CategoryID = Category.ID;
